8690ddb5654a24e4797925b6f170154eace1a3ed
TitanDevX <46249816+TitanDevX@users.noreply.github.com>
2024-11-11 18:44:42 +0300

    Handle cases when player is teleporting

--- src/main/kotlin/me/titan/blockpartyevents/cmds/event/EventLeaveCmd.kt
+++ src/main/kotlin/me/titan/blockpartyevents/cmds/event/EventLeaveCmd.kt
@@ -3,8 +3,10 @@ package me.titan.blockpartyevents.cmds.event
 import me.titan.blockpartyevents.cmds.EventCmds
 import me.titan.blockpartyevents.cmds.acceptedEvents
 import me.titan.blockpartyevents.eventManager
+import me.titan.blockpartyevents.manager.PlayerManager
 import me.titan.blockpartyevents.model.event.EventType
 import me.titan.blockpartyevents.permissionPrefix
+import me.titan.blockpartyevents.playerManager
 import me.titan.blockpartyevents.util.StringMapping
 import me.titan.blockpartyevents.util.tell
 import me.titan.titanlib.commands.SubCommand
@@ -17,7 +19,12 @@ class EventLeaveCmd(cmd: EventCmds): SubCommand("leave"
     , "","$permissionPrefix.leave",1,false,cmd) {
     override fun execute(s: CommandSender, args: Array<out String>) {
 
-        eventManager().leaveEvent(s as Player)
+        val p = s as Player
+        if(!p.hasMetadata(PlayerManager.PLAYER_EVENT_METADATA)){
+            playerManager().handlePlayerLeaveNotInEvent(p)
+            return
+        }
+        eventManager().leaveEvent(p)
 
     }
 }
