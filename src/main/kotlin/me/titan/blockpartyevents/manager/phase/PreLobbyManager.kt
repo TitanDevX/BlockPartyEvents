a3bdf0c082954e0908cc91ed411e237bf4d7587f
TitanDevX <46249816+TitanDevX@users.noreply.github.com>
2024-11-11 18:47:38 +0300

    Register last location

--- src/main/kotlin/me/titan/blockpartyevents/manager/phase/PreLobbyManager.kt
+++ src/main/kotlin/me/titan/blockpartyevents/manager/phase/PreLobbyManager.kt
@@ -4,7 +4,9 @@ import me.titan.blockpartyevents.api.event.EventCancelCause
 import me.titan.blockpartyevents.config.messages.Messages
 import me.titan.blockpartyevents.model.event.BREvent
 import me.titan.blockpartyevents.model.event.EventState
+import me.titan.blockpartyevents.playerManager
 import me.titan.blockpartyevents.tasks.PreLobbyCountdown
+import me.titan.titanlib.common.Common
 import me.titan.titanlib.common.TimeUtil
 import org.bukkit.entity.Player
 import org.bukkit.event.Event
@@ -56,6 +58,13 @@ class PreLobbyManager(event: BREvent): EventPhaseManager(event) {
             return false;
         }
         player.inventory.clear()
+        playerManager().teleportToEvent(player,event.info.spawns.get("global_lobby")!!).whenComplete { t, u ->
+            if(u != null){
+                event.leavePlayer(player)
+                Common.tell(player,"&cThere was an error teleporting you to event so you were removed from it.")
+
+            }
+           }
         return true
     }
 
